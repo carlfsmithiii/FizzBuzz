6f903cee0bc277ca2412d4036fb77242b2f45b50
Carl Smith <carlfsmithiii@gmail.com>
2018-08-01 14:58:13 -0400

    BONUS: prime numbers have borders.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let fizzbuzz = (upperLimit) => {
+function fizzbuzz(upperLimit) {
+    let arrayOfPrimeIndexes = generateArrayOfPrimes(upperLimit);
     let fullString = "";
     for (let i = 1; i <= upperLimit; i++) {
         let elementString = i.toString();
@@ -8,6 +9,9 @@ let fizzbuzz = (upperLimit) => {
         if (i % 3 === 0) {
             elementString = "<span style='color:red'>" + elementString + "</span>";
         }
+        if (arrayOfPrimeIndexes[i]) {
+            elementString = "<span style='border:1px solid black'>" + elementString + "</span>";   
+        }
         if (i !== upperLimit) {
             elementString += ', ';
         }
@@ -15,6 +19,22 @@ let fizzbuzz = (upperLimit) => {
     }
 
     document.write("<p>" + fullString + "</p>");
+}
+
+// Sieve of Eratosthenes
+function generateArrayOfPrimes(upperLimit) {
+    let isTheIndexPrime = [0]; // 0 is not prime
+    for (let i = 1; i <= upperLimit; i++) {
+        isTheIndexPrime.push(true);
+    }
+    for (let i = 2; i * i <= upperLimit; i++) {
+        if (isTheIndexPrime[i]) {
+            for (let j = i; i * j <= upperLimit; j++) {
+                isTheIndexPrime[i * j] = false;
+            }
+        }
+    } 
+    return isTheIndexPrime;
 }
 
 fizzbuzz(1000);
